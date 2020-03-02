e8ce6ba5d8ba825760c6d2fd9d8ebd20a291477f
TeckFreak <rahulvashistha96@gmail.com>
2020-03-02 10:56:50 +0530

    Added helmet library to increase security.

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 import express = require("express");
 import * as bodyParser from "body-parser";
 import { MainRoute } from "./routes/main.route";
+import helmet = require('helmet');
 
-class App
-{
+class App {
     public app: express.Application = express();
     private router: MainRoute;
 
-    constructor()
-    {
+    constructor() {
         this.config();
 
         this.router = new MainRoute(this.app);
         this.router.initRouters();
     }
 
-    private config(): void
-    {
+    private config(): void {
+        // Secure application from well-known vulnerabilities. URL: https://helmetjs.github.io/docs/
+        this.app.use(helmet());
         // support application/json
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
