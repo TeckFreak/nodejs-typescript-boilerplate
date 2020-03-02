e76efc692fb7b764d0cd1494d487863b329a7342
TeckFreak <rahulvashistha96@gmail.com>
2020-03-02 10:41:43 +0530

    Added method to control different request.

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,24 +1,31 @@
 import { Request, Response } from 'express';
 
-export class MainController
-{
-    /**
-     * Shows the welcome message of the routed page when POST request is sent.
-     * @param req Request received.
-     * @param res Response to send.
-     */
-    public showWelcomeMessageOnPost(req: Request, res: Response)
-    {
-        res.status(200).send("<h1>Welcome on the POST page.</h1>");
-    }
-
+export class MainController {
     /**
      * Shows the welcome message of the routed page when GET request is sent.
      * @param req Request received.
      * @param res Response to send.
      */
-    public showWelcomeMessageOnGet(req: Request, res: Response)
-    {
-        res.status(200).send("<h1>Welcome on the GET page</h1>");
+    public showHomePage(req: Request, res: Response) {
+        res.status(200).json({
+            success: true,
+            requestMethod: req.method
+        });
+    }
+
+    public showPOSTPage(req: Request, res: Response) {
+        res.status(200).send("<h1>" + req.method + " Request Handled</h1>");
+    }
+
+    public showGETPage(req: Request, res: Response) {
+        res.status(200).send("<h1>" + req.method + " Request Handled</h1>");
+    }
+
+    public showPUTPage(req: Request, res: Response) {
+        res.status(200).send("<h1>" + req.method + " Request Handled</h1>");
+    }
+
+    public showDELETEPage(req: Request, res: Response) {
+        res.status(200).send("<h1>" + req.method + " Request Handled</h1>");
     }
 }
